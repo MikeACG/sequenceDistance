f070695a97ce616d8dcdc80be7ea5aa706a0eafb
MikeACG <A01270966@exatec.tec.mx>
2021-12-01 20:09:32 -0600

    fixed ranges intersection bug

--- intersect.h
+++ intersect.h
@@ -1,9 +1,9 @@
 // determines if two ranges have at least one overlapping position
 bool intersect(int xstart, int xend, int ystart, int yend) {
 
-    if (ystart >= xstart && ystart <= xend) {
+    if (xstart >= ystart && xstart <= yend) {
         return true;
-    } else if (yend >= xstart && yend <= xend) {
+    } else if (xend >= ystart && xend <= yend) {
         return true;
     }
 
